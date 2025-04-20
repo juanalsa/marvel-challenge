a6a081098058bc1317ddcbfc79f50f556fc455f2
${'Julian Alvarado'} <juanalsa@gmail.com>
2025-04-20 13:11:27 -0500

    Add user interaction logging interceptor and authentication retrieval method

---
+++ src/main/java/com/test/api/marvel_challenge/web/interceptor/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.test.api.marvel_challenge.web.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private UserInteractionInterceptor userInteractionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInteractionInterceptor);
+    }
+}
